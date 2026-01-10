a8b2afef63f578621fa61ed7bdaa985ffd7f7ee6
PurpleHorrorRus <purplehorrorrus@gmail.com>
2026-01-10 21:37:03 +0800

    fix: Dockerfile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -144,7 +144,7 @@ export default defineNuxtConfig({
 	},
 
 	runtimeConfig: {
-		cookieKey: process.env.NUXT_COOKIE_KEY || "",
+		cookieKey: (process.env.NUXT_COOKIE_KEY || "").replace(/\\n/g, "\n"),
 
 		session: {
 			password: process.env.NUXT_SESSION_PASSWORD || "",
